162118ff32c096b2e79355d2ea7c5adf4bdfa9e4
Annett-a <solo280306@mail.ru>
2025-06-09 20:19:48 +0300

    13.05.25

---
+++ 13.05.25/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int findPeak(int[] a) {
+        int low = 0;
+        int high = a.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (a[mid] < a[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите количество элементов: ");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        System.out.println("Введите элементы массива (битоническая последовательность):");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        int peakIndex = findPeak(a);
+        System.out.println("Индекс вершины: " + peakIndex);
+        System.out.println("Значение в вершине: " + a[peakIndex]);
+    }
+}
