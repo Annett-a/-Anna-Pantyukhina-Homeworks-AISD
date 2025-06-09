de9e3305e223c4f3b55ec5753f0934e7192df996
Annett-a <solo280306@mail.ru>
2025-06-09 20:51:49 +0300

    fix

--- 13.05.25/src/Main.java
+++ 13.05.25/src/Main.java
@@ -21,7 +21,7 @@ public class Main {
         int n = sc.nextInt();
         int[] a = new int[n];
 
-        System.out.println("Введите элементы массива (битоническая последовательность):");
+        System.out.println("Введите элементы массива:");
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
